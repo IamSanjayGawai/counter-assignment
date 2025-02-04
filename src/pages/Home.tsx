741a900a7dda190148f39dc9d60d44abe33a73d9
Sanjay Gawai <sanjaygawai1002@gmail.com>
2025-02-04 16:09:55 +0530

    added all component in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import { Container, Grid, Paper, Typography } from "@mui/material";
 import Counter from "../components/Counter";
+import UserForm from "../components/UserForm";
+import RichTextEditor from "../components/RichTextEditor";
+import Dashboard from "../components/Dashboard";
 
 const Home = () => {
   return (
@@ -16,7 +19,26 @@ const Home = () => {
           </Paper>
         </Grid>
 
-      
+        {/* User Form */}
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 4 }}>
+            <UserForm />
+          </Paper>
+        </Grid>
+
+        {/* Rich Text Editor */}
+        <Grid item xs={12}>
+          <Paper sx={{ p: 4 }}>
+            <RichTextEditor />
+          </Paper>
+        </Grid>
+
+        {/* Dashboard Section */}
+        <Grid item xs={12}>
+          <Paper sx={{ p: 4 }}>
+            <Dashboard />
+          </Paper>
+        </Grid>
       </Grid>
     </Container>
   );
